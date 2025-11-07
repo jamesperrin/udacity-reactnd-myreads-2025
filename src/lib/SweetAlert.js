41de50cbd89441ea635b776c87bc905ae9c74e65
James Perrin <1738333+jamesperrin@users.noreply.github.com>
2025-11-07 12:49:47 -0800

    Refac: Add parameter cb for call back to include validation check

--- src/lib/SweetAlert.js
+++ src/lib/SweetAlert.js
@@ -2,7 +2,7 @@ import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 const MySwal = withReactContent(Swal);
 
-export const showSwalError = (title, text) => {
+export const showSwalError = (title, text, cb) => {
   MySwal.fire({
     title: title ?? 'Error!',
     text: text ?? 'Something went wrong.',
@@ -10,11 +10,12 @@ export const showSwalError = (title, text) => {
     confirmButtonText: 'OK',
     // Prevent SweetAlert from immediately re‑focusing the confirm button again
     // when it closes (extra safety; not strictly required)
-    //didClose: () => {
-    // Defer to ensure SweetAlert's focus trap has been fully released
-    // requestAnimationFrame(() => {
-    // });
-    //},
+    didClose: () => {
+      if (cb && typeof cb === 'function') {
+        // Defer to ensure SweetAlert's focus trap has been fully released
+        requestAnimationFrame(() => {});
+      }
+    },
   });
 };
 
